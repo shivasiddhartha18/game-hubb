8cdea5b92580bc385736c3aa579af156c8c771e7
Shiva <siddhartha.shiva18@gmail.com>
2024-02-01 07:11:18 -0500

    Game Critic Score

---
+++ src/components/CriticScore.tsx
@@ -0,0 +1,16 @@
+import { Badge } from "@chakra-ui/react";
+
+interface Props {
+  score: number;
+}
+
+const CriticScore = ({ score }: Props) => {
+  const color = score > 75 ? "green" : score > 60 ? "yellow" : "";
+  return (
+    <Badge colorScheme={color} fontSize={12} paddingX={2} borderRadius={2}>
+      {score}
+    </Badge>
+  );
+};
+
+export default CriticScore;
