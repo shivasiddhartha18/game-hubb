1a4d85acae21dd406a6ba6653c67c11790f7fa90
Shiva <siddhartha.shiva18@gmail.com>
2024-02-08 08:11:19 -0500

    sort selector and No Image handling

--- src/components/SortSelector.tsx
+++ src/components/SortSelector.tsx
@@ -2,16 +2,39 @@ import { Button, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
 import React from "react";
 import { BsChevronDown } from "react-icons/bs";
 
-const SortSelector = () => {
+interface Props {
+  onSelectSortOrder: (sortOrder: string) => void;
+  sortOrder: string;
+}
+
+const SortSelector = ({ onSelectSortOrder, sortOrder }: Props) => {
+  const sortOrders = [
+    { value: "", label: "Relevence" },
+    { value: "-added", label: "Date Added" },
+    { value: "name", label: "Name" },
+    { value: "-released", label: "Dare Release" },
+    { value: "-metacritic", label: "Popularity" },
+    { value: "-rating", label: "Average Rating" },
+  ];
+
+  const currentSortorder = sortOrders.find(
+    (order) => order.value === sortOrder
+  );
   return (
     <Menu>
       <MenuButton marginY={2} as={Button} rightIcon={<BsChevronDown />}>
-        Sort
+        Order By: {currentSortorder?.label || "Relevence"}
       </MenuButton>
       <MenuList>
-        <MenuItem>Name</MenuItem>
-        <MenuItem>Relevence</MenuItem>
-        <MenuItem>Date</MenuItem>
+        {sortOrders.map((order) => (
+          <MenuItem
+            onClick={() => onSelectSortOrder(order.value)}
+            key={order.value}
+            value={order.value}
+          >
+            {order.label}
+          </MenuItem>
+        ))}
       </MenuList>
     </Menu>
   );
--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,7 +1,10 @@
-const getCroppedImageurl = (url: string) =>{
-   const target = "media/"
-    const index = url.indexOf(target) + target.length;
-    return url.slice(0, index) + 'crop/600/400/' + url.slice(index);
-}
+import NoImage from "../assets/No-Image-Placeholder.svg.png";
+
+const getCroppedImageurl = (url: string) => {
+  if (!url) return NoImage;
+  const target = "media/";
+  const index = url.indexOf(target) + target.length;
+  return url.slice(0, index) + "crop/600/400/" + url.slice(index);
+};
 
 export default getCroppedImageurl;
